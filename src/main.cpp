eacabb00609416d5e7fdffd8d0434f8bee4af723
Patrik Kavan <patrik.kavan2@gmail.com>
2026-04-04 14:38:46 +0200

    Updated CMakePresets

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include "StrikeEngine.h"
 
 int main(int argc, char** argv) {
-    Strike::Application app;
-    Strike::Window& window = app.getWindow();
-    window.setWindowTitle("SpaceRacer");
-    window.setSize(1280, 720);
+    try {
+        Strike::Application app;
+        Strike::Window& window = app.getWindow();
+        window.setWindowTitle("SpaceRacer");
+        window.setSize(1280, 720);
 
-    Strike::World::get().loadScene("assets/scenes/menu.xml");
+        Strike::World::get().loadScene("assets/scenes/menu.xml");
 
-    app.run();
+        app.run();
+    }
+    catch (const std::runtime_error& e) {
+        return 1;
+    }
 
     return 0;
 }
